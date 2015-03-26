6297c6e0fbe3e3b2a6b145b23217af0f9f1e8b9c
Mehdi <azzabi08@gmail.com>
2015-03-26 19:31:17 +0100

    Create View Produit

---
+++ src/tn/tunisieTelecom/mPayment/gestionStatistique/azbs/web/ctr/ProduitCtr.java
@@ -0,0 +1,95 @@
+package tn.tunisieTelecom.mPayment.gestionStatistique.azbs.web.ctr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.entity.Produit;
+import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.entity.SousCategories;
+import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.local.services.ProduitEJBLocal;
+import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.local.services.SousCategotiesEJBLocal;
+
+
+@ManagedBean
+@SessionScoped
+public class ProduitCtr {
+	private List<Produit> produits = new ArrayList<Produit>();
+	private Produit selectedProduit = new Produit();
+	private Produit produit = new Produit();
+	
+	int idSousCat;
+	
+	@EJB
+	ProduitEJBLocal produitEJBLocal ;
+	@EJB
+	SousCategotiesEJBLocal sousCategotiesEJBLocal;
+	
+	public List<Produit> doFindall(){
+		return produits= produitEJBLocal.findall();
+	}
+	
+	public void add (){
+		
+		produit.setSousCategories(sousCategotiesEJBLocal.findById(idSousCat));
+		for (Produit produit : produits) {
+			if(produit.getId()== this.produit.getId()){
+				System.err.println("duplicate id for produit");
+				
+				return;
+			}
+		}
+		
+		produitEJBLocal.add(produit);
+		produit = new Produit();
+	}
+
+	public void update(){
+		produitEJBLocal.update(selectedProduit);
+	}
+	public void delete(){
+		produitEJBLocal.delete(selectedProduit);
+	}
+	public List<Produit> getProduits() {
+		return produits;
+	}
+
+	public void setProduits(List<Produit> produits) {
+		this.produits = produits;
+	}
+
+	public Produit getSelectedProduit() {
+		return selectedProduit;
+	}
+
+	public void setSelectedProduit(Produit selectedProduit) {
+		this.selectedProduit = selectedProduit;
+	}
+
+
+	public int getIdSousCat() {
+		return idSousCat;
+	}
+
+
+	public void setIdSousCat(int idSousCat) {
+		this.idSousCat = idSousCat;
+	}
+
+
+	public Produit getProduit() {
+		return produit;
+	}
+
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+
+
+	
+	
+}
