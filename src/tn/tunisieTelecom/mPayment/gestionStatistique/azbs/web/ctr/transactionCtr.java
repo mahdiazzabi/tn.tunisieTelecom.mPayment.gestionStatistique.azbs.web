64d894c7f61d66d1c3486638f11049b4959a5b9a
Mehdi <azzabi08@gmail.com>
2015-03-31 14:31:44 +0200

    Update transactionCtr with read file and persist <<< !! Le fichier upload ne passe pas de la vue au controlleur a corriger !! >>

--- src/tn/tunisieTelecom/mPayment/gestionStatistique/azbs/web/ctr/transactionCtr.java
+++ src/tn/tunisieTelecom/mPayment/gestionStatistique/azbs/web/ctr/transactionCtr.java
@@ -10,6 +10,9 @@ import java.util.StringTokenizer;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.servlet.http.Part;
+
+import org.primefaces.model.UploadedFile;
 
 import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.entity.Banque;
 import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.entity.Transaction;
@@ -22,10 +25,10 @@ import tn.tunisieTelecom.mPayment.gestionStatistique.azbs.ejb.local.services.Tra
 @SessionScoped
 public class transactionCtr {
 	
-	private File file;
+	private File file ;
 	int idBanque ;
 	Transaction tr ;
-	List<Transaction> transactions = new ArrayList<Transaction>();
+	List<Transaction> transactions ;
 	
 	
 	@EJB
@@ -37,10 +40,20 @@ public class transactionCtr {
 	@EJB
 	ProduitEJBLocal produitEJBLocal;
 	
-	
-	void traitement_Fichier_STB_BH_BNA(){
+	 private static String getFilename(Part part) {  
+	        for (String cd : part.getHeader("content-disposition").split(";")) {  
+	            if (cd.trim().startsWith("filename")) {  
+	                String filename = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");  
+	                return filename.substring(filename.lastIndexOf('/') + 1).substring(filename.lastIndexOf('\\') + 1); // MSIE fix.  
+	            }  
+	        }  
+	        return null;  
+	    }  
+	List<Transaction> traitement_Fichier_STB_BH_BNA(){
+		transactions =  new ArrayList<Transaction>() ;
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-y");
 		try {
+			    
 			InputStream ips=new FileInputStream(file.getName()); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
@@ -80,15 +93,16 @@ public class transactionCtr {
 
 			System.out.println(e.getMessage());
 		}
-		
+		return transactions ;
 
 	}
-	void traitement_Fichier_ATB(){
+	List<Transaction> traitement_Fichier_ATB(){
+
+		transactions =  new ArrayList<Transaction>() ;
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
 		Banque banque = new Banque() ;
 		banque = banqueEJBLocal.findById(idBanque);
 		try {
-
 			InputStream ips=new FileInputStream(file.getName()); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
@@ -128,38 +142,84 @@ public class transactionCtr {
 
 			System.out.println(e.getMessage());
 	}
+		return transactions ;
 	}
-	void traitement_Fichier_SMT(){
+	
+	List<Transaction> traitement_Fichier_SMT(){
 
+		transactions =  new ArrayList<Transaction>() ;
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
 		Banque banque = new Banque() ;
 		banque = banqueEJBLocal.findById(idBanque);
 		try {
+			InputStream ips=new FileInputStream(file.getName()); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			br.readLine();
+			while ((ligne=br.readLine())!=null){
+				System.out.println(ligne);
+				//etat
+				System.out.println(ligne.substring(0, 1));
+				String etat = ligne.substring(0, 1);
+				//id transaction
+				System.out.println(ligne.substring(1, 14));
+				String id_trans = ligne.substring(1, 14);
+				//date
+				System.out.println(ligne.substring(14, 28));
+				Date date_trans = formatter.parse(ligne.substring(14, 28));
+				
+				
+				System.out.println(ligne.substring(28, 43));
+				
+				System.out.println(ligne.substring(43, 55));
+				String tel_source = ligne.substring(43, 55);
+				
+				System.out.println(ligne.substring(55, 67));
+				Double montant = Double.parseDouble(ligne.substring(55, 67));			
+
+				tr = new Transaction();
+				tr.setBanque(banque);
+				tr.setEtat(etat);
+				tr.setId_transaction(id_trans);
+				tr.setDate(date_trans);
+				tr.setMontant(montant);
+				tr.setTel_source(tel_source);
+				System.out.println(tr.getId());
+				System.out.println(tr.getEtat());
+				System.out.println(tr.getId_transaction());
+				System.out.println(tr.getTel_benef());
+				System.out.println(tr.getTel_source());
+				System.out.println(tr.getBanque().getNom());
+				System.out.println(tr.getDate());
+				System.out.println(tr.getMontant());
+				System.out.println(tr.getProduit().getLibelle());
+				transactions.add(tr);
+				}
 			
 			
 		} catch (Exception e) {
 
 			System.out.println(e.getMessage());
 		}
-		
+		return transactions;
 	}
 
 	public void traiterFichier() throws IOException {
 		System.out.println("OKKKKKKKK");
-		System.out.println(idBanque);
-		
 		
 		try {
 			switch(idBanque){
-			case 1 : case 2 :  case 3 : traitement_Fichier_STB_BH_BNA();break;
-			case 4 : traitement_Fichier_ATB();break;
-			case 5 : traitement_Fichier_SMT();break;
+			case 1 : case 2 :  case 3 :  transactionEJBLocal.addListe(traitement_Fichier_STB_BH_BNA()) ;break;
+			case 4 : transactionEJBLocal.addListe(traitement_Fichier_ATB()) ; break;
+			case 5 : case 6 :transactionEJBLocal.addListe( traitement_Fichier_SMT()); break ;
 			default:;
 		}
 		
 		
 		} catch (Exception e) {
-
+			System.out.println("NOT OKKKKKKKK");
+			
 			System.out.println(e.getMessage());
 		}
 		
@@ -179,6 +239,7 @@ public class transactionCtr {
 
 	public void setFile(File file) {
 		this.file = file;
+	
 	}
 	
 
